b88bc5bf377a81220b733499df0a41496784c6d7
JimMicko <jimfalar@gmail.com>
2023-04-23 19:15:27 +0800

    Update Files
    Update Files

--- scripts/route/field/FIELD 1.js
+++ scripts/route/field/FIELD 1.js
@@ -7,6 +7,6 @@ fetch('https://script.google.com/macros/s/AKfycbwZVluh7Q6rfJolALqmRv6dyloH8QwnCE
         return prev + `<tr>${td.join("")}</tr>`
     }, "\r")
     // console.log(tr)
-    document.getElementById("dashboard").innerHTML = tr;
+    document.getElementById("fieldDashboardList").innerHTML = tr;
 })
 
